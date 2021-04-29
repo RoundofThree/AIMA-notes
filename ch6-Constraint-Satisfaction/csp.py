0fa239555ad499983bb3741388507755368d1c11
RoundofThree <zhuoyingjiangli@gmail.com>
2021-04-30 00:44:29 +0200

    Add CSP notes

---
+++ ch6-Constraint-Satisfaction/csp.py
@@ -0,0 +1,26 @@
+from utils import * 
+
+# Node consistency
+
+# AC3
+
+# PC2
+
+# Alldiff constraint propagation 
+
+# Atmost constraint propagation
+
+# Bounds propagation 
+
+# CSP solver with backtracking 
+
+# CSP with conflict directed backjumping 
+
+# CSP solver with min conflicts 
+
+# CSP tree solver
+
+# Cutset conditioning 
+
+# Tree decomposition 
+
