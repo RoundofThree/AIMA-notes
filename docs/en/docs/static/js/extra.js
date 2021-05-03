c0335c3c255eb465b6b0eac96c86758fcb8b87dc
RoundofThree <zhuoyingjiangli@gmail.com>
2021-05-03 18:36:18 +0200

    Add javascript

--- docs/en/docs/static/js/extra.js
+++ docs/en/docs/static/js/extra.js
@@ -1,3 +1,26 @@
+window.MathJax = {
+    tex2jax: {
+      inlineMath: [ ["\\(","\\)"] ],
+      displayMath: [ ["\\[","\\]"] ]
+    },
+    TeX: {
+      TagSide: "right",
+      TagIndent: ".8em",
+      MultLineWidth: "85%",
+      equationNumbers: {
+        autoNumber: "AMS",
+      },
+      unicode: {
+        fonts: "STIXGeneral,'Arial Unicode MS'"
+      }
+    },
+    showProcessingMessages: false,
+    messageStyle: "none"
+  };
 
-document.body.dataset.mdColorScheme = "default";
-document.body.dataset.mdColorPrimary = "light-blue";
+window.addEventListener('load', function() { 
+    document.body.setAttribute('data-md-color-scheme', 'default');
+    document.body.setAttribute('data-md-color-primary', 'light-blue');
+}, false);
+
+pangu.spacingPageBody();
